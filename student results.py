b27217875adb7dcaf0ecef3ab179ac09afe351c5
Ponnaboina Vijay Kumar Yadav <192411135.simats@saveetha.com>
2025-06-18 11:24:24 +0530

    Create student results.py

---
+++ student results.py
@@ -0,0 +1,8 @@
+marks= float(input("enter the student marks: "))
+
+# check whether the is pass/fail
+
+if marks >=50:
+    print("the student has passed.")
+else:
+    print("the student has failed.")
